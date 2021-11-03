71d5f5aaa6f408437fccc9f3e51fea3cd08afa7f
Sergey Simenshteyn <simenshteyn@gmail.com>
2021-11-03 22:20:23 +0700

    Add nested routing with blueprints for Flask

---
+++ src/api/common.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+from api.v1.routes import v1
+
+api = Blueprint('api', __name__, url_prefix='/api')
+
+api.register_blueprint(v1)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask
+from api.common import api
 
 app = Flask(__name__)
 
-
-@app.route('/api/v1/')
-def index():
-    return jsonify(result="Hello, World!")
-
+app.register_blueprint(api, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
