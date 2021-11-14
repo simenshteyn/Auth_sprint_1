39271f8863ba9fec089de5b48d06421cf1bfd48b
Sergey Simenshteyn <simenshteyn@gmail.com>
2021-11-14 13:32:46 +0700

    Add role creation endpoint fully working
    Took 37 minutes

--- src/api/v1/role/routes.py
+++ src/api/v1/role/routes.py
@@ -1,14 +1,46 @@
+from http import HTTPStatus
+
 from dependency_injector.wiring import inject, Provide
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, make_response, request
+from pydantic import BaseModel, ValidationError
 
 from core.containers import Container
+from core.utils import make_service_exception
 from services.role import RoleService
 
 role = Blueprint('role', __name__, url_prefix='/role')
 
 
-@role.route('/')
+class RoleCreationRequest(BaseModel):
+    role_name: str
+
+
+@role.route('/', methods=['GET'])
 @inject
 def get_roles(role_service: RoleService = Provide[Container.role_service]):
     role_list = role_service.get_roles_list()
     return jsonify(role_list)
+
+
+@role.route('/', methods=['POST'])
+@inject
+def create_role(role_service: RoleService = Provide[Container.role_service]):
+    request_json = request.json
+    try:
+        create_request = RoleCreationRequest(**request_json)
+    except ValidationError as err:
+        service_exception = make_service_exception(err)
+        return make_response(
+            jsonify(service_exception),
+            HTTPStatus.BAD_REQUEST
+        )
+
+    try:
+        uuid, role_name = role_service.create_role(create_request.role_name)
+    except Exception as err:
+        return make_response(jsonify(str(err)), HTTPStatus.BAD_REQUEST)
+
+    return make_response(
+        jsonify(uuid=uuid, role_name=role_name),
+        HTTPStatus.OK
+    )
--- src/services/role.py
+++ src/services/role.py
@@ -1,3 +1,4 @@
+from core.utils import ServiceException
 from models.role import Role
 from db.pg import db
 
@@ -7,8 +8,18 @@ class RoleService:
         pass
 
     def get_roles_list(self):
-        # Example:
-        # new_role = Role(role_name='subscriber')
-        # db.session.add(new_role)
-        # db.session.commit()
         return Role.query.all()
+
+    def create_role(self, role_name: str):
+        existing_role: Role = Role.query.filter(
+            Role.role_name == role_name).first()
+
+        if existing_role:
+            error_code = 'ROLE_EXISTS'
+            message = 'Role with than name already exists'
+            raise ServiceException(error_code=error_code, message=message)
+
+        new_role = Role(role_name=role_name)
+        db.session.add(new_role)
+        db.session.commit()
+        return new_role.role_id, new_role.role_name
